3ca62a712a7f0719387d7e6f24e9f9a50c84ebbd
imperuminate <imperuminate@gmail.com>
2024-08-22 01:21:03 +0200

    commit with 4 lesson

---
+++ from_okulik/homework/homework_3/task_4.py
@@ -0,0 +1,6 @@
+from math import sqrt
+
+с_1 = 4
+с_2 = 7
+
+print(sqrt((с_1 * с_1) + (с_2 * с_2)))
---
+++ from_okulik/homework/homework_3/task_5.py
@@ -0,0 +1,4 @@
+cube_length = 7
+
+print('Обьем куба:', (cube_length ** 3)) 
+print('Площадь куба:', (6 * cube_length ** 2)) 
